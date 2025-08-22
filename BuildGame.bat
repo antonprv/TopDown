72da61bbee9ce744d61c1a4bb0eff4b51ab71f58
anton.prv <anton.prv@yandex.com>
2025-08-22 21:25:13 +0400

    First project commit

---
+++ BuildGame.bat
@@ -0,0 +1,6 @@
+"E:\UE_Other\UE_4.23\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="D:\Projects\TopDown\TopDown.uproject" ^
+-platform=Win32 -clientconfig=Development ^
+-archivedirectory="D:\Projects\TopDown\Packaged\FromBat" ^
+-cook -stage -archive -pak -package -allmaps
